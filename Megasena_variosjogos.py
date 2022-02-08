fb919174933066336cff81eb1eaafcdaf7bdecf0
zito <zitotreverson@gmail.com>
2022-02-08 07:45:16 -0300

    v6 - opção continuar sim ou não incluido + jogos sorted()

--- Megasena_variosjogos.py
+++ Megasena_variosjogos.py
@@ -1,4 +1,5 @@
-# MEGA SENA -
+# MEGA SENA ____  Gerador aleatório de jogos
+# por: github.com/zittox-
 
 import random
 import time
@@ -15,6 +16,11 @@ class Mega:
     @classmethod
     def mostra_jogo(self):
         jogo = [random.sample(range(1, 61), self.quantinum) for i in range(self.numjog)]
+        while jogo:
+            for i in jogo:
+                i.sort()
+            break
+        jogo.sort()
         jogox = ""
         while jogo:
             for i in jogo:
@@ -34,14 +40,14 @@ class Mega:
     @classmethod
     def perg1(self):
          while True:
-            self.quantinum = input("\n\nQuantos números tem o jogo da MEGA SENA que você quer?\ndigite uma opção: 6, 7, 8 ou 9\n")
+            self.quantinum = input("\nQuantos números tem o jogo da MEGA SENA que você quer?\ndigite uma opção: 6, 7, 8 ou 9\n")
             try:
                 self.quantinum = int(self.quantinum)
                 if self.quantinum == 6 or self.quantinum == 7 or self.quantinum == 8 or self.quantinum == 9:
-                    print("\n~~~~>> Você escolheu ter", self.quantinum, "números em cada jogo ----~!~~!~~!\n")
+                    print(f"\n~~~~>> Você escolheu ter {self.quantinum} números em cada jogo ----~!~~!~~!\n")
                     break
                 elif self.quantinum != range(6,10):
-                    print("\n!!!!! >>>> Escolha um número entre 6 e 9 <<<< !!!\n")
+                    print("\n!!!!! >>>> Escolha um número entre 6 e 9 <<<< !!!")
                     self.perg1()
             except ValueError:
                 print("\n!!!!! >>>> PRESTENÇÃO: escolha um número <<<< !!!!")
