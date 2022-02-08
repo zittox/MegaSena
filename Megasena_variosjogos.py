ee3652ae592bd064c298b0c8d9da9fd2df2d2457
zito <zitotreverson@gmail.com>
2022-02-08 06:34:01 -0300

    v5 - opção continuar sim ou não incluido <

--- Megasena_variosjogos.py
+++ Megasena_variosjogos.py
@@ -24,11 +24,12 @@ class Mega:
 
         if self.numjog == 1:
             print(f'O jogo de {self.quantinum} números é :\n{jogox}')
+            self.pergunta_sim_nao("Gostaria de continuar?")
         else:
             print(f'Os {self.numjog} jogos de {self.quantinum} números são:\n\nCALCULAND0 ~~~~ └[∵┌]└[ ∵ ]┘[┐∵]┘\n')
             time.sleep(2)
-            print(f'{jogox}\n\n_/\_ Boa sorte!')
-
+            print(f'{jogox}\n')
+            self.pergunta_sim_nao("Gostaria de continuar?")
 
     @classmethod
     def perg1(self):
@@ -45,7 +46,7 @@ class Mega:
             except ValueError:
                 print("\n!!!!! >>>> PRESTENÇÃO: escolha um número <<<< !!!!")
                 self.perg1()
-            return int(self.quantinum)
+            return self.quantinum
 
 
     @classmethod
@@ -59,7 +60,7 @@ class Mega:
                     print("\nO máximo são 100 jogadas <<<--------\n")
                     self.perg2()
                 elif self.numjog >= 1 and self.numjog <= 100:
-                    print(f"\n~~~~>> Você escolheu  {self.numjog} jogos\n")
+                    print(f"\n~~~~>> Você escolheu {self.numjog} jogos\n")
                 break
             except ValueError:
                 print("\n!!!!! >>>> PRESTENÇÃO: digite um número entre 1 e 100 <<<< !!!!\n")
@@ -67,15 +68,26 @@ class Mega:
 
             return self.numjog
 
+    @classmethod
+    def pergunta_sim_nao(self, menssagem):
+
+        resposta = ""
+        while resposta not in ("s", "n"):
+            resposta = input(f"\n{menssagem} (s ou n):")
+            if resposta not in ("s", "n"):
+                print("\nPor favor, digite sim \"s\" ou não \"n\"")
+            elif resposta == "n":
+                print("\n_/\_ Boa sorte!\n >>>>> Até a próxima!!!")
+                break
+            else:
+                self.perg1()
+                self.perg2()
+                self.mostra_jogo()
 
 
 
 
-Mega.perg1()
-Mega.perg2()
-Mega.mostra_jogo()
-
-
+Mega.pergunta_sim_nao(" ~~~~~~ Bem vindo __/\__\nGostaria de fazer jogo(s) da Mega Sena?")
 
 
 
