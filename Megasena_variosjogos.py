65e770f8910724d3d8208508df33c688bd6488e6
Ozzy Scuba <48163195+ozzysp@users.noreply.github.com>
2022-02-09 14:45:19 -0300

    Staging (#1)
    * Minimal corrections
    
    * Linha de teste adicionada.
    
    * PEP8 format corrections
    
    * Adequação do código ao padrão PEP8

--- Megasena_variosjogos.py
+++ Megasena_variosjogos.py
@@ -4,18 +4,18 @@
 import random
 import time
 
-class Mega:
 
+class Mega:
     quantinum = int()
     numjog = int()
 
-    def __init__(self,numjog,quantinum):
+    def __init__(self, numjog, quantinum):
         self.numjog = numjog
         self.quantinum = quantinum
 
     @classmethod
-    def mostra_jogo(self):
-        jogo = [random.sample(range(1, 61), self.quantinum) for i in range(self.numjog)]
+    def mostra_jogo(cls):
+        jogo = [random.sample(range(1, 61), cls.quantinum) for _ in range(cls.numjog)]
         while jogo:
             for i in jogo:
                 i.sort()
@@ -28,73 +28,67 @@ class Mega:
                 jogox = "\n".join(str(i) for i in jogo)
             break
 
-        if self.numjog == 1:
-            print(f'O jogo de {self.quantinum} números é :\n{jogox}')
-            self.pergunta_sim_nao("Gostaria de continuar?")
+        if cls.numjog == 1:
+            print(f'O jogo de {cls.quantinum} números é :\n{jogox}')
+            cls.pergunta_sim_nao("Gostaria de continuar?")
         else:
-            print(f'Os {self.numjog} jogos de {self.quantinum} números são:\n\nCALCULAND0 ~~~~ └[∵┌]└[ ∵ ]┘[┐∵]┘\n')
+            print(f'Os {cls.numjog} jogos de {cls.quantinum} números são:\n\nCALCULAND0 ~~~~ └[∵┌]└[ ∵ ]┘[┐∵]┘\n')
             time.sleep(2)
             print(f'{jogox}\n')
-            self.pergunta_sim_nao("Gostaria de continuar?")
+            cls.pergunta_sim_nao("Gostaria de continuar?")
 
     @classmethod
-    def perg1(self):
-         while True:
-            self.quantinum = input("\nQuantos números tem o jogo da MEGA SENA que você quer?\ndigite uma opção: 6, 7, 8 ou 9\n")
+    def perg1(cls):
+        while True:
+            cls.quantinum = input(
+                "\nQuantos números tem o jogo da MEGA SENA que você quer?\ndigite uma opção: 6, 7, 8 ou 9\n")
             try:
-                self.quantinum = int(self.quantinum)
-                if self.quantinum == 6 or self.quantinum == 7 or self.quantinum == 8 or self.quantinum == 9:
-                    print(f"\n~~~~>> Você escolheu ter {self.quantinum} números em cada jogo ----~!~~!~~!\n")
+                cls.quantinum = int(cls.quantinum)
+                if cls.quantinum == 6 or cls.quantinum == 7 or cls.quantinum == 8 or cls.quantinum == 9:
+                    print(f"\n~~~~>> Você escolheu ter {cls.quantinum} números em cada jogo ----~!~~!~~!\n")
                     break
-                elif self.quantinum != range(6,10):
+                elif cls.quantinum != range(6, 10):
                     print("\n!!!!! >>>> Escolha um número entre 6 e 9 <<<< !!!")
-                    self.perg1()
+                    cls.perg1()
             except ValueError:
                 print("\n!!!!! >>>> PRESTENÇÃO: escolha um número <<<< !!!!")
-                self.perg1()
-            return self.quantinum
-
+                cls.perg1()
+            return cls.quantinum
 
     @classmethod
-    def perg2(self):
+    def perg2(cls):
 
         while True:
-            self.numjog = input(f"Quantos jogos você gostaria de ter com {self.quantinum} números?\n")
+            cls.numjog = input(f"Quantos jogos você gostaria de ter com {cls.quantinum} números?\n")
             try:
-                self.numjog = int(self.numjog)
-                if self.numjog > 100:
+                cls.numjog = int(cls.numjog)
+                if cls.numjog > 100:
                     print("\nO máximo são 100 jogadas <<<--------\n")
-                    self.perg2()
-                elif self.numjog >= 1 and self.numjog <= 100:
-                    print(f"\n~~~~>> Você escolheu {self.numjog} jogos\n")
+                    cls.perg2()
+                elif 1 <= cls.numjog <= 100:
+                    print(f"\n~~~~>> Você escolheu {cls.numjog} jogos\n")
                 break
             except ValueError:
                 print("\n!!!!! >>>> PRESTENÇÃO: digite um número entre 1 e 100 <<<< !!!!\n")
-                self.perg2()
+                cls.perg2()
 
-            return self.numjog
+            return cls.numjog
 
     @classmethod
-    def pergunta_sim_nao(self, menssagem):
+    def pergunta_sim_nao(cls, menssagem):
 
         resposta = ""
         while resposta not in ("s", "n"):
-            resposta = input(f"\n{menssagem} (s ou n):")
+            resposta = input(f"\n{menssagem} (s ou n): ")
             if resposta not in ("s", "n"):
                 print("\nPor favor, digite sim \"s\" ou não \"n\"")
             elif resposta == "n":
-                print("\n_/\_ Boa sorte!\n >>>>> Até a próxima!!!")
+                print("\n__ Boa sorte!\n >>>>> Até a próxima!!!")
                 break
             else:
-                self.perg1()
-                self.perg2()
-                self.mostra_jogo()
+                cls.perg1()
+                cls.perg2()
+                cls.mostra_jogo()
 
 
-
-
-Mega.pergunta_sim_nao(" ~~~~~~ Bem vindo __/\__\nGostaria de fazer jogo(s) da Mega Sena?")
-
-
-
-
+Mega.pergunta_sim_nao(" ~~~~~~ Bem vindo ____\nGostaria de fazer jogo(s) da Mega Sena?")
