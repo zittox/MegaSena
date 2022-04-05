2cf9903e707ecafbc017136312b7b325d5f851dd
zito <zitotreverson@gmail.com>
2022-04-05 03:14:24 -0300

    + rich print

--- megasena_br.py
+++ megasena_br.py
@@ -1,3 +1,4 @@
+
 # MEGA SENA ____  Gerador aleatório de jogos
 # por: github.com/zittox
 # colaborador: github.com/ozzysp
@@ -7,6 +8,14 @@
 import time
 import qrandom
 import requests
+from rich import print as rpri
+from rich.progress import track
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+
 
 
 
@@ -18,37 +27,38 @@ class Mega:
 
     def perg1():
         while True:
-            quantinum = input("\nQuantos números tem o jogo da MEGA SENA que você quer?\ndigite uma opção: 6, 7, 8 ou 9\n")
+            quantinum = console.input("[bold white on blue]\nQuantos números tem o jogo da MEGA SENA que você quer?[/]\n[bold italic red]digite uma opção: 6, 7, 8 ou 9\n")
             try:
                 quantinum = int(quantinum)
                 if quantinum >= 6 < 10:
-                    print(f"\n~~~~>> Você escolheu ter {quantinum} números em cada jogo ----~!~~!~~!\n")
+                    rpri(f"[bold white on blue]\n~~~~>> Você escolheu ter {quantinum} números em cada jogo ----~!~~!~~!\n")
                     break
                 else:
-                    print("\n!!!!! >>>> Escolha um número entre 6 e 9 <<<< !!!")
+                    rpri("\n[bold italic red]!!!!! >>>> Escolha um número entre 6 e 9 <<<< !!!")
                     Mega.perg1()
             except ValueError:
-                print("\n!!!!! >>>> PRESTENÇÃO: escolha um número <<<< !!!!")
+                rpri("\n[bold italic red]!!!!! >>>> PRESTENÇÃO: escolha um número <<<< !!!!")
                 Mega.perg1()
             return quantinum
 
         def perg2():
+
             while True:
-                numjog = input(f"Quantos jogos você gostaria de ter com {quantinum} números?\n")
+                numjog = console.input(f"[bold white on blue]Quantos jogos você gostaria de ter com {quantinum} números?\n")
                 try:
                     numjog = int(numjog)
                     if numjog == 0:
-                        print("\n!!!!! >>>> ZERO jogadas não é um número válido neh, tenta de novo! <<<< !!!!\n")
-                        Mega.perg2()
+                        rpri("\n[bold italic red]!!!!! >>>> ZERO jogadas não é um número válido neh, tenta de novo! <<<< !!!!\n")
+                        perg2()
                     if numjog > 100:
-                        print("\nO máximo são 100 jogadas <<<--------\n")
-                        Mega.perg2()
+                        rpri("\n[bold italic red]O máximo são 100 jogadas <<<--------\n")
+                        perg2()
                     elif numjog >= 1 and numjog <= 100:
-                        print(f"\n~~~~>> Você escolheu {numjog} jogos\n")
+                        rpri(f"[bold white on blue]\n~~~~>> Você escolheu {numjog} jogos\n")
                     break
                 except ValueError:
-                    print("\n!!!!! >>>> PRESTENÇÃO: digite um número entre 1 e 100 <<<< !!!!\n")
-                    Mega.perg2()
+                    rpri("\n[bold italic red]!!!!! >>>> PRESTENÇÃO: digite um número entre 1 e 100 <<<< !!!!\n")
+                    perg2()
                 return numjog
 
             def mostra_jogo():
@@ -68,20 +78,25 @@ class Mega:
                     break
 
                 if numjog == 1:
-                    print(f'O jogo de {quantinum} número é :\n{jogox2}')
+                    console.print(f'[bold white on blue]O jogo de {quantinum} número é :\n{jogox2}', justify="center", overflow="fold", style="bold")
                     pergunta_sim_nao("Gostaria de continuar?\n")
                 else:
-                    print(f'Os {numjog} jogos de {quantinum} números são:\n\nCALCULAND0 ~~~~ └[∵┌]└[ ∵ ]┘[┐∵]┘\n')
-                    time.sleep(2)
-                    print(f'{jogox2}\n')
+                    console.print(f'[bold white on blue]Os {numjog} jogos de {quantinum} números são:\n\n', justify="center", overflow="fold", style="bold")
+
+                    for _ in track(range(numjog), description="CALCULAND0 ~~~~ └[∵┌]└[ ∵ ]┘[┐∵]┘\n", style="cornflower_blue"):
+                        time.sleep(0.1)
+                    console.print(f'{jogox2}\n', justify="center", overflow="fold", style="bold")
                     pergunta_sim_nao("Gostaria de continuar?\n")
             mostra_jogo()
         perg2()
 
 
 class Result:
+    def __init__(self):
+        pass
 
     def ultimo_resultado():
+
         apicaixa = 'https://loteriascaixa-api.herokuapp.com/api/mega-sena/latest'
         r = requests.get(apicaixa).json()
         data = f"Concurso: {r['concurso']}"
@@ -101,33 +116,44 @@ class Result:
             data3 = "Acumulou: Não"
         data4 = f"Prox_prêmio: {r['acumuladaProxConcurso']}"
         data5 = f"Prox_concurso: {r['dataProxConcurso']}"
-        print(f'{data}\n{data1}\n{data2}\n{venc}\n{prem}\n{data3}\n{data4}\n{data5}')
-        pergunta_sim_nao("Gostaria de continuar?")
+        console.print(f'[blue1]{data}[/]\n[light_slate_blue]{data1}[/]\n[chartreuse3]{data2}[/]\n[yellow2]{venc}[/]\n[red3]{prem}[/]\n[light_slate_blue]{data3}[/]\n[red3]{data4}[/]\n[light_slate_blue]{data5}[/]')
+        pergunta_sim_nao("Gostaria de continuar? ")
 
     def qual_concurso():
-        conc = input("Digite o número do concurso que deseja pesquisar:\n")
+        conc = console.input("[bold white on blue]Digite o número do concurso que deseja pesquisar:\n")
         apicaixaconc = f'https://loteriascaixa-api.herokuapp.com/api/mega-sena/{conc}'
-        r = requests.get(apicaixaconc).json()
-        data = f"Concurso: {r['concurso']}"
-        data1 = f"Data: {r['data']}"
-        data2 = f"Dezenas: {r['dezenas']}"
-        if r['premiacoes'][0]['vencedores'] == 0:
-            venc = f"Vencedores: Não"
-        else:
-            venc = f"Vencedores: {r['premiacoes'][0]['vencedores']}"
-        if r['premiacoes'][0]['premio'] == "-":
-            prem = f"Premiações: 0"
-        else:
-            prem = f"Premiações: R$ {r['premiacoes'][0]['premio']}"
-        if r['acumulou'] == True:
-            data3 = "Acumulou: Sim"
-        else:
-            data3 = "Acumulou: Não"
-        data4 = f"Prox_prêmio: {r['acumuladaProxConcurso']}"
-        data5 = f"Prox_concurso: {r['dataProxConcurso']}"
-        print(f'{data}\n{data1}\n{data2}\n{venc}\n{prem}\n{data3}\n{data4}\n{data5}')
-        pergunta_sim_nao("Gostaria de continuar?")
+        if conc.isnumeric():
 
+            try:
+                r = requests.get(apicaixaconc).json()
+                data = f"Concurso: {r['concurso']}"
+                data1 = f"Data: {r['data']}"
+                data2 = f"Dezenas: {r['dezenas']}"
+                if r['premiacoes'][0]['vencedores'] == 0:
+                    venc = f"Vencedores: Não"
+                else:
+                    venc = f"Vencedores: {r['premiacoes'][0]['vencedores']}"
+                if r['premiacoes'][0]['premio'] == "-":
+                    prem = f"Premiações: 0"
+                else:
+                    prem = f"Premiações: R$ {r['premiacoes'][0]['premio']}"
+                if r['acumulou'] == True:
+                    data3 = "Acumulou: Sim"
+                else:
+                    data3 = "Acumulou: Não"
+                data4 = f"Prox_prêmio: {r['acumuladaProxConcurso']}"
+                data5 = f"Prox_concurso: {r['dataProxConcurso']}"
+                console.print(f'[blue1]{data}[/]\n[light_slate_blue]{data1}[/]\n[chartreuse3]{data2}[/]\n[yellow2]{venc}[/]\n[red3]{prem}[/]\n[light_slate_blue]{data3}[/]\n[red3]{data4}[/]\n[light_slate_blue]{data5}[/]')
+                pergunta_sim_nao("Gostaria de continuar? ")
+            except ValueError:
+                console.print(f"[bold italic green on red blink]Concurso não encontrado!\n")
+
+                pergunta_sim_nao("Para tentar de novo ")
+        else:
+            console.print(f"[bold italic green on red blink]Você não digitou número, digite números válidos!\n")
+
+            pergunta_sim_nao("Para tentar de novo ")
+                
 
 
 
@@ -138,14 +164,23 @@ class Result:
 def pergunta_sim_nao(menssagem):
     resposta = ""
     while resposta not in ("s", "n"):
-        resposta = input(f"\n{menssagem}digite: s ou n\n")
+        resposta = console.input(f"[bold white on blue]\n{menssagem}digite: s ou n\n")
         if resposta not in ("s", "n"):
-            print("\nVoçê tem que usar as letras \"s\" ou \"n\"\n")
+            console.print("\n[bold italic green on red blink]Voçê tem que usar as letras \"s\" ou \"n\"\n", justify="center")
             pergunta_sim_nao("Se quiser começar ")
         elif resposta == "s":
-            resp2 = input("< j > Gostaria de fazer jogo(s) da Mega Sena?\n< r > Gostaria de ver o último resultado?\n< p > Gostaria de pesquisar por número de concurso?\ndigite j, r ou p\n")
+
+            table = Table(title=' ', style="deep_sky_blue1")
+            table.add_column("Opções", style="green1", no_wrap=True)
+            table.add_column("Funções", style="turquoise2", no_wrap=True, justify="center")
+            table.add_row(" J ", "Fazer jogo(s) da Mega Sena")
+            table.add_row(" R ", "Ver o resultado do último concurso")
+            table.add_row(" P ", "Pesquisar por número de concurso")
+            console.print(table)
+            resp2 = console.input("[bold white on blue]\nDigite a opção desejada:\n")
+
             if resp2 not in ("j", "r", "p"):
-                print("\nVoçê tem que usar as letras \"j\", \"r\" ou \"p\":")
+                console.print("[bold italic green on red blink]\nVoçê tem que usar as letras \"j\", \"r\" ou \"p\":", justify="center")
                 pergunta_sim_nao("Para tentar de novo ")
             if resp2 == "j":
                 Mega.perg1()
@@ -153,14 +188,16 @@ def pergunta_sim_nao(menssagem):
                 Result.ultimo_resultado()
             if resp2 == "p":
                 Result.qual_concurso()
+
         else:
-           print("\n__ Boa sorte!\n >>>>> Até a próxima!!!")
-           break
+            console.print("[bold green on black]\n__ Boa sorte!\n >>>>> Até a próxima!!!\n\n", justify="full")
+            break
 
 
 
 
 
-pergunta_sim_nao("~~~~~~ Bem vindo ____\nBora começar!?!?\n")
+
+pergunta_sim_nao('\n\n~~~~~~ Bem vindo __________\nBora começa?!?!?\n')
 
 
